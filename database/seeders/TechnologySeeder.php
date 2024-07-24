b676c6afdb0c3f10e993855bd14b3dde1caf6e96
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-24 17:32:33 +0200

    [UPD] creazione nuova tabella + seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //per evitare problemi di chiavi
+        //disabilito chiavi nel caso ci sono
+        Schema::disableForeignKeyConstraints();
+        //svuoto tabella
+        Technology::truncate();
+        //inserisco i campi ciclando sugli elemnti
+        $technologies = ['HTML', 'CSS', 'JavaScript', 'Vue', 'Bootstrap', 'PHP', 'Laravel', 'MySQL'];
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::of($technology)->slug();
+            //salvo
+            $new_technology->save();
+        }
+
+
+        Schema::enableForeignKeyConstraints();
     }
 }
